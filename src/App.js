699fb370a31973520a4e4063ed3d6bef3ec48995
srijana <srijana.thulung123@gmail.com>
2022-07-18 10:25:10 +0545

    modal practise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Modal from './components/Modal';
+import {useState} from 'react';
 
 function App() {
+
+  const openModalHandler=()=>{
+    setOpenModal(true);
+  }
+
+  const[openModal,setOpenModal]=useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-     learn react
-      </header>
+     <h1>Hey click on the button to open a modal</h1>
+     <button className='openModalBtn'
+      onClick={openModalHandler}
+      >Open</button>
+      {openModal && <Modal closeModal={setOpenModal}/>}
+      {/* <Modal/> */}
     </div>
   );
 }
